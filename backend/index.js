eb885d05f9767c4fe15d2eee98f57835990db8e6
Sourav553 <souravsamanta57378@gmail.com>
2024-12-15 21:31:58 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-// const bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
 const app = express()
 const Routes = require("./routes/route.js")
 
@@ -10,11 +10,19 @@ const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
-// app.use(bodyParser.json({ limit: '10mb', extended: true }))
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+app.use(bodyParser.json({ limit: '10mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: 'https://school-management-system-ruby.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true // if cookies are involved
+  }));
+  
+  // Default CORS headers for all routes
+  app.options('*', cors());
 
 mongoose
     .connect(process.env.MONGO_URL, {
